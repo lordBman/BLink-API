5af072ad5bc537c499d806cc2a17bfd2573f31d0
lordBman <okelekelenobel@gmail.com>
2026-06-01 05:43:15 +0100

    added bundler

---
+++ src/build.ts
@@ -0,0 +1,30 @@
+import { Glob } from "bun";
+import JetLogger from "jet-logger";
+
+const glob = new Glob("*.ts");
+const path = "./src/pages/scripts";
+const files = Array.from(glob.scanSync(path));
+
+if (files.length === 0) {
+    JetLogger.warn("No entry points found in the ./pages/scripts directory.");
+    process.exit(0);
+}
+
+JetLogger.info(`Found ${files.length} entry points: ${files.join(", ")}`);
+Bun.build({
+    entrypoints: files.map(file => `${path}/${file}`),
+    outdir: './assets/scripts',
+    minify: true,
+    sourcemap: 'linked',
+    splitting: false,
+    naming: {
+        entry: '[name].[ext]',
+        chunk: '[dir]/chunks/[name].[ext]',
+        asset: '[dir]/assets/[name]-[hash].[ext]'
+    }
+}).then((init)=>{
+    if (!init.success) {
+        JetLogger.err(`Build failed: ${init.logs}`);
+        process.exit(1);
+    }
+});
--- src/pages/components/contianer.tsx
+++ src/pages/components/contianer.tsx
@@ -1,10 +1,16 @@
 import { Html } from '@elysia/html'
 
-const Contianer = (props: { tilte: string, children: JSX.Element | JSX.Element[] }) =>{
+const Contianer = (props: { tilte: string, headerStyles?: string[], headerScripts?: string[], children: JSX.Element | JSX.Element[] }) =>{
     return (
         <html lang="en">
             <head>
                 <title>{props.tilte}</title>
+                {props.headerStyles?.map((style) => (
+                    <link rel="stylesheet" href={`/assets/styles/${style}`} />
+                ))}
+                {props.headerScripts?.map((script) => (
+                    <script src={`/assets/scripts/${script}`} defer />
+                ))}
             </head>
             <body>{props.children}</body>
         </html>
